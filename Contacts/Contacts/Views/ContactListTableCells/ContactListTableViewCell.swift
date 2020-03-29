018b95e23fda97864f835b261cee262fee0c1fa3
Tania Jasam <taniajasam@Tanias-MacBook-Air.local>
2020-03-29 19:15:15 +0530

    UI design in progress

---
+++ Contacts/Contacts/Views/ContactListTableCells/ContactListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactListTableViewCell.swift
+//  Contacts
+//
+//  Created by Tania Jasam on 29/03/20.
+//  Copyright © 2020 Tania Jasam. All rights reserved.
+//
+
+import UIKit
+
+class ContactListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
